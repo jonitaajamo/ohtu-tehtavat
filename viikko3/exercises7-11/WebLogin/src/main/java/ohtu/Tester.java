316625f2e5469760f16d51beb5423928e8a70f1a
Joni Taajamo <joni.taajamo@outlook.com>
2019-11-18 23:19:48 +0200

    exercises 7-8 done

---
+++ viikko3/exercises7-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -0,0 +1,84 @@
+package ohtu;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Random;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:4567");
+        
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
+
+        element.click();
+//
+//        sleep(2);
+//
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//
+//        sleep(2);
+//        element.submit();
+//
+//        sleep(3);
+//
+//        driver.quit();
+
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("pekkis");
+//        element = driver.findElement(By.name("login"));
+//
+//        sleep(2);
+//        element.submit();
+//
+//        sleep(3);
+
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("arto"+r.nextInt(100000));
+
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("pekkis");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("pekkis");
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+        element.submit();
+
+        element = driver.findElement(By.xpath("//a[@href='ohtu']"));
+        element.click();
+
+        sleep(3);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        sleep(3);
+        driver.quit();
+
+
+
+    }
+    
+    private static void sleep(int n){
+        try{
+            Thread.sleep(n*1000);
+        } catch(Exception e){}
+    }
+}
